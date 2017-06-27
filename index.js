a335a50cacb8a4ff9a888c851b045c8d6bbdafb1
Titus Wormer <tituswormer@gmail.com>
2017-06-27 19:24:03 +0200

    More randomness, and patterns

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ var Finger = require('fingerprintjs2');
 var patterns = [
   lines,
   dots,
-  squares
+  squares,
+  crosses,
+  cloth,
+  zigzag
 ];
 
 var id = localStorage.i;
@@ -19,7 +22,7 @@ if (id) {
 }
 
 function next() {
-  localStorage.t = (Number(localStorage.t) || 0) + Math.floor(Math.random() * 100);
+  localStorage.t = (Number(localStorage.t) || 0) + Math.floor(Math.random() * 360);
   window.location.reload();
 }
 
@@ -30,8 +33,8 @@ function onfingersuccess(id) {
 
 function paint(id) {
   var salt = (Number(localStorage.t) || 0);
-  var int = Math.floor(parseInt(id, 16) / 1e32) + salt;
-  var hue = int % 360 || 0;
+  var int = Math.floor(parseInt(id, 16) / 1e31) + salt;
+  var hue = (int % 360) || 0;
   var hl = 'hsl(' + hue + ', 97%, 43%)';
   var bg = 'hsl(' + (hue - (((int % 11) + 1) * 15)) + ', 95%, 20%)';
   var fg = 'hsl(' + (hue + (((int % 9) + 1) * 15)) + ', 95%, 99%)';
@@ -63,7 +66,7 @@ function paint(id) {
 }
 
 function lines(id, fg, bg) {
-  var size = (10 + ((id % 8) * 10)) / 2;
+  var size = 4 + (id % 180);
 
   return [
     'background-color: ' + bg,
@@ -72,7 +75,7 @@ function lines(id, fg, bg) {
 }
 
 function dots(id, fg, bg) {
-  var size = ((id % 8) + 1) * 15;
+  var size = 4 + (id % 180);
 
   return [
     'background-color: ' + bg,
@@ -86,7 +89,7 @@ function dots(id, fg, bg) {
 }
 
 function squares(id, fg, bg) {
-  var size = ((id % 8) + 1) * 15;
+  var size = 4 + (id % 180);
 
   return [
     'background-color: ' + bg,
@@ -95,6 +98,65 @@ function squares(id, fg, bg) {
       'linear-gradient(45deg, ' + fg + ' 25%, transparent 25%, transparent 75%, ' + fg + ' 75%, ' + fg + ')'
     ].join(', '),
     'background-size: ' + (size * 2) + 'px ' + (size * 2) + 'px',
-    'background-position: 0 0, ' + size + 'px ' + size + 'px'
+    'background-position: ' + [
+      '0 0',
+      size + 'px ' + size + 'px'
+    ].join(', ')
+  ].join(';');
+}
+
+function crosses(id, fg, bg) {
+  var size = 4 + (id % 180);
+  var dbl = size * 2;
+  var secondary = Math.max(1, Math.floor(size / 25));
+  var primary = secondary * 2;
+
+  return [
+    'background: ' + [
+      'radial-gradient(circle, transparent 20%, ' + bg + ' 20%, ' + bg + ' 80%, transparent 80%, transparent)',
+      'radial-gradient(circle, transparent 20%, ' + bg + ' 20%, ' + bg + ' 80%, transparent 80%, transparent) ' + size + 'px ' + size + 'px',
+      'linear-gradient(' + fg + ' ' + primary + 'px, transparent ' + primary + 'px) 0 -' + secondary + 'px',
+      'linear-gradient(90deg, ' + fg + ' ' + primary + 'px, transparent ' + primary + 'px) -' + secondary + 'px 0'
+    ].join(', '),
+    'background-color: ' + bg,
+    'background-size: ' + [
+      dbl + 'px ' + dbl + 'px',
+      dbl + 'px ' + dbl + 'px',
+      size + 'px ' + size + 'px',
+      size + 'px ' + size + 'px'
+    ].join(', ')
+  ].join(';');
+}
+
+function cloth(id, fg, bg) {
+  var size = 4 + (id % 180);
+
+  fg = fg.replace(/hsl/, 'hsla').replace(/\)/, ', 0.5)');
+
+  return [
+    'background-color: ' + bg,
+    'background-image:' + [
+      'linear-gradient(90deg, ' + fg + ' 50%, transparent 50%)',
+      'linear-gradient(' + fg + ' 50%, transparent 50%)'
+    ],
+    'background-size: ' + size + 'px ' + size + 'px'
+  ].join(';');
+}
+
+function zigzag(id, fg, bg) {
+  var size = 1 + (id % 180);
+  var dbl = size * 2;
+
+  fg = fg.replace(/hsl/, 'hsla').replace(/\)/, ', 0.5)');
+
+  return [
+    'background:' + [
+      'linear-gradient(135deg, ' + fg + ' 25%, transparent 25%) -' + size + 'px 0',
+      'linear-gradient(225deg, ' + fg + ' 25%, transparent 25%) -' + size + 'px 0',
+      'linear-gradient(315deg, ' + fg + ' 25%, transparent 25%)',
+      'linear-gradient(45deg, ' + fg + ' 25%, transparent 25%)'
+    ],
+    'background-color: ' + bg,
+    'background-size: ' + dbl + 'px ' + dbl + 'px'
   ].join(';');
 }
