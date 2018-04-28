80411b14d67792cb18cd8b7edb7fc2d4f39d0fe8
Titus Wormer <tituswormer@gmail.com>
2018-04-28 17:19:07 +0200

    Refactor code-style

--- build.js
+++ build.js
@@ -1,20 +1,20 @@
-'use strict';
+'use strict'
 
-var vfile = require('to-vfile');
-var report = require('vfile-reporter');
-var h = require('hastscript');
-var select = require('hast-util-select').select;
-var unified = require('unified');
-var markdown = require('remark-parse');
-var style = require('remark-preset-wooorm');
-var links = require('remark-validate-links');
-var remark2rehype = require('remark-rehype');
-var doc = require('rehype-document');
-var slug = require('rehype-slug');
-var min = require('rehype-preset-minify');
-var favicon = require('rehype-prevent-favicon-request');
-var html = require('rehype-stringify');
-var pack = require('./package.json');
+var vfile = require('to-vfile')
+var report = require('vfile-reporter')
+var h = require('hastscript')
+var select = require('hast-util-select').select
+var unified = require('unified')
+var markdown = require('remark-parse')
+var style = require('remark-preset-wooorm')
+var links = require('remark-validate-links')
+var remark2rehype = require('remark-rehype')
+var doc = require('rehype-document')
+var slug = require('rehype-slug')
+var min = require('rehype-preset-minify')
+var favicon = require('rehype-prevent-favicon-request')
+var html = require('rehype-stringify')
+var pack = require('./package.json')
 
 unified()
   .use(markdown)
@@ -32,42 +32,43 @@ unified()
   .use(min)
   .use(favicon)
   .use(html)
-  .process(vfile.readSync('readme.md'), function (err, file) {
-    console.error(report(err || file));
+  .process(vfile.readSync('readme.md'), function(err, file) {
+    console.error(report(err || file))
     if (file) {
-      file.dirname = 'static';
-      file.basename = 'index.html';
-      vfile.writeSync(file);
+      file.dirname = 'static'
+      file.basename = 'index.html'
+      vfile.writeSync(file)
     }
-  });
+  })
 
 function nojs() {
-  var script = 'document.body.className = \'js\'; document.body.removeChild(document.currentScript)';
-  return transformer;
+  var script =
+    "document.body.className = 'js'; document.body.removeChild(document.currentScript)"
+  return transformer
   function transformer(tree) {
-    var body = select('body', tree);
-    var props = body.properties;
+    var body = select('body', tree)
+    var props = body.properties
 
     if (!('className' in props)) {
-      props.className = [];
+      props.className = []
     }
 
-    props.className.push('nojs');
+    props.className.push('nojs')
 
     body.children.unshift({
       type: 'element',
       tagName: 'script',
       properties: {},
       children: [{type: 'text', value: script}]
-    });
+    })
   }
 }
 
 function avatar() {
-  return transformer;
+  return transformer
   function transformer(tree) {
-    select('body', tree).children.unshift(h('figure', [
-      h('img', {src: './pinguin.png'})
-    ]));
+    select('body', tree).children.unshift(
+      h('figure', [h('img', {src: './pinguin.png'})])
+    )
   }
 }
--- index.js
+++ index.js
@@ -1,163 +1,226 @@
-'use strict';
+'use strict'
 
 /* eslint-env browser */
 
-var Finger = require('fingerprintjs2');
+var Finger = require('fingerprintjs2')
 
-var patterns = [
-  lines,
-  dots,
-  squares,
-  crosses,
-  cloth,
-  zigzag
-];
+var patterns = [lines, dots, squares, crosses, cloth, zigzag]
 
-var id = localStorage.i;
+var id = localStorage.i
 
 if (id) {
-  paint(id);
+  paint(id)
 } else {
-  new Finger({detectScreenOrientation: false}).get(onfingersuccess);
+  new Finger({detectScreenOrientation: false}).get(onfingersuccess)
 }
 
 function next() {
-  localStorage.t = (Number(localStorage.t) || 0) + Math.floor(Math.random() * 360);
-  window.location.reload();
+  localStorage.t =
+    (Number(localStorage.t) || 0) + Math.floor(Math.random() * 360)
+  window.location.reload()
 }
 
 function onfingersuccess(id) {
-  localStorage.i = id;
-  paint(id);
+  localStorage.i = id
+  paint(id)
 }
 
 function paint(id) {
-  var salt = (Number(localStorage.t) || 0);
-  var int = Math.floor(parseInt(id, 16) / 1e31) + salt;
-  var hue = (int % 360) || 0;
-  var hl = 'hsl(' + hue + ', 97%, 43%)';
-  var bg = 'hsl(' + (hue - (((int % 11) + 1) * 15)) + ', 95%, 20%)';
-  var fg = 'hsl(' + (hue + (((int % 9) + 1) * 15)) + ', 95%, 99%)';
+  var salt = Number(localStorage.t) || 0
+  var int = Math.floor(parseInt(id, 16) / 1e31) + salt
+  var hue = int % 360 || 0
+  var hl = 'hsl(' + hue + ', 97%, 43%)'
+  var bg = 'hsl(' + (hue - (int % 11 + 1) * 15) + ', 95%, 20%)'
+  var fg = 'hsl(' + (hue + (int % 9 + 1) * 15) + ', 95%, 99%)'
 
-  var style = document.createElement('style');
-  var p = document.createElement('p');
-  var small = document.createElement('small');
-  var button = document.createElement('button');
-  var code = document.createElement('code');
+  var style = document.createElement('style')
+  var p = document.createElement('p')
+  var small = document.createElement('small')
+  var button = document.createElement('button')
+  var code = document.createElement('code')
 
-  style.appendChild(document.createTextNode([
-    'a, button { color: ' + hl + '; border-color: currentcolor }',
-    'html { ' + patterns[(int % patterns.length) || 0](int, fg, bg) + ' }',
-    'body { background-color: ' + fg + '; color: ' + bg + ' }',
-    'figure::after {background: radial-gradient(circle, transparent, ' + bg + ')  }'
-  ].join('\n')));
+  style.appendChild(
+    document.createTextNode(
+      [
+        'a, button { color: ' + hl + '; border-color: currentcolor }',
+        'html { ' + patterns[int % patterns.length || 0](int, fg, bg) + ' }',
+        'body { background-color: ' + fg + '; color: ' + bg + ' }',
+        'figure::after {background: radial-gradient(circle, transparent, ' +
+          bg +
+          ')  }'
+      ].join('\n')
+    )
+  )
 
-  p.appendChild(small);
-  small.appendChild(document.createTextNode('P.S. I made this site for you, '));
-  small.appendChild(code);
-  small.appendChild(document.createTextNode(', try a '));
-  small.appendChild(button);
-  code.appendChild(document.createTextNode(id.slice(0, 6)));
-  button.appendChild(document.createTextNode('different design'));
-  button.addEventListener('click', next);
-  code.title = id + (salt ? '@' + salt.toString(16) : '');
+  p.appendChild(small)
+  small.appendChild(document.createTextNode('P.S. I made this site for you, '))
+  small.appendChild(code)
+  small.appendChild(document.createTextNode(', try a '))
+  small.appendChild(button)
+  code.appendChild(document.createTextNode(id.slice(0, 6)))
+  button.appendChild(document.createTextNode('different design'))
+  button.addEventListener('click', next)
+  code.title = id + (salt ? '@' + salt.toString(16) : '')
 
-  document.body.insertBefore(p, document.getElementsByTagName('p')[0]);
-  document.head.appendChild(style);
+  document.body.insertBefore(p, document.getElementsByTagName('p')[0])
+  document.head.appendChild(style)
 }
 
 function lines(id, fg, bg) {
-  var size = 4 + (id % 180);
+  var size = 4 + id % 180
 
   return [
     'background-color: ' + bg,
-    'background-image: repeating-linear-gradient(' + ((id % 4) * 45) + 'deg, transparent, transparent ' + size + 'px, ' + fg + ' ' + size + 'px, ' + fg + ' ' + (size * 2) + 'px);'
-  ].join(';');
+    'background-image: repeating-linear-gradient(' +
+      (id % 4) * 45 +
+      'deg, transparent, transparent ' +
+      size +
+      'px, ' +
+      fg +
+      ' ' +
+      size +
+      'px, ' +
+      fg +
+      ' ' +
+      size * 2 +
+      'px);'
+  ].join(';')
 }
 
 function dots(id, fg, bg) {
-  var size = 4 + (id % 180);
+  var size = 4 + id % 180
 
   return [
     'background-color: ' + bg,
-    'background-image: ' + [
-      'radial-gradient(' + fg + ' 15%, transparent 16%)',
-      'radial-gradient(' + fg + ' 15%, transparent 16%)'
-    ].join(', '),
-    'background-size: ' + (size * 2) + 'px ' + (size * 2) + 'px',
+    'background-image: ' +
+      [
+        'radial-gradient(' + fg + ' 15%, transparent 16%)',
+        'radial-gradient(' + fg + ' 15%, transparent 16%)'
+      ].join(', '),
+    'background-size: ' + size * 2 + 'px ' + size * 2 + 'px',
     'background-position: 0 0, ' + size + 'px ' + size + 'px'
-  ].join(';');
+  ].join(';')
 }
 
 function squares(id, fg, bg) {
-  var size = 4 + (id % 180);
+  var size = 4 + id % 180
 
   return [
     'background-color: ' + bg,
-    'background-image: ' + [
-      'linear-gradient(45deg, ' + fg + ' 25%, transparent 25%, transparent 75%, ' + fg + ' 75%, ' + fg + ')',
-      'linear-gradient(45deg, ' + fg + ' 25%, transparent 25%, transparent 75%, ' + fg + ' 75%, ' + fg + ')'
-    ].join(', '),
-    'background-size: ' + (size * 2) + 'px ' + (size * 2) + 'px',
-    'background-position: ' + [
-      '0 0',
-      size + 'px ' + size + 'px'
-    ].join(', ')
-  ].join(';');
+    'background-image: ' +
+      [
+        'linear-gradient(45deg, ' +
+          fg +
+          ' 25%, transparent 25%, transparent 75%, ' +
+          fg +
+          ' 75%, ' +
+          fg +
+          ')',
+        'linear-gradient(45deg, ' +
+          fg +
+          ' 25%, transparent 25%, transparent 75%, ' +
+          fg +
+          ' 75%, ' +
+          fg +
+          ')'
+      ].join(', '),
+    'background-size: ' + size * 2 + 'px ' + size * 2 + 'px',
+    'background-position: ' + ['0 0', size + 'px ' + size + 'px'].join(', ')
+  ].join(';')
 }
 
 function crosses(id, fg, bg) {
-  var size = 4 + (id % 180);
-  var dbl = size * 2;
-  var secondary = Math.max(1, Math.floor(size / 25));
-  var primary = secondary * 2;
+  var size = 4 + id % 180
+  var dbl = size * 2
+  var secondary = Math.max(1, Math.floor(size / 25))
+  var primary = secondary * 2
 
   return [
-    'background: ' + [
-      'radial-gradient(circle, transparent 20%, ' + bg + ' 20%, ' + bg + ' 80%, transparent 80%, transparent)',
-      'radial-gradient(circle, transparent 20%, ' + bg + ' 20%, ' + bg + ' 80%, transparent 80%, transparent) ' + size + 'px ' + size + 'px',
-      'linear-gradient(' + fg + ' ' + primary + 'px, transparent ' + primary + 'px) 0 -' + secondary + 'px',
-      'linear-gradient(90deg, ' + fg + ' ' + primary + 'px, transparent ' + primary + 'px) -' + secondary + 'px 0'
-    ].join(', '),
+    'background: ' +
+      [
+        'radial-gradient(circle, transparent 20%, ' +
+          bg +
+          ' 20%, ' +
+          bg +
+          ' 80%, transparent 80%, transparent)',
+        'radial-gradient(circle, transparent 20%, ' +
+          bg +
+          ' 20%, ' +
+          bg +
+          ' 80%, transparent 80%, transparent) ' +
+          size +
+          'px ' +
+          size +
+          'px',
+        'linear-gradient(' +
+          fg +
+          ' ' +
+          primary +
+          'px, transparent ' +
+          primary +
+          'px) 0 -' +
+          secondary +
+          'px',
+        'linear-gradient(90deg, ' +
+          fg +
+          ' ' +
+          primary +
+          'px, transparent ' +
+          primary +
+          'px) -' +
+          secondary +
+          'px 0'
+      ].join(', '),
     'background-color: ' + bg,
-    'background-size: ' + [
-      dbl + 'px ' + dbl + 'px',
-      dbl + 'px ' + dbl + 'px',
-      size + 'px ' + size + 'px',
-      size + 'px ' + size + 'px'
-    ].join(', ')
-  ].join(';');
+    'background-size: ' +
+      [
+        dbl + 'px ' + dbl + 'px',
+        dbl + 'px ' + dbl + 'px',
+        size + 'px ' + size + 'px',
+        size + 'px ' + size + 'px'
+      ].join(', ')
+  ].join(';')
 }
 
 function cloth(id, fg, bg) {
-  var size = 4 + (id % 180);
+  var size = 4 + id % 180
 
-  fg = fg.replace(/hsl/, 'hsla').replace(/\)/, ', 0.5)');
+  fg = fg.replace(/hsl/, 'hsla').replace(/\)/, ', 0.5)')
 
   return [
     'background-color: ' + bg,
-    'background-image:' + [
-      'linear-gradient(90deg, ' + fg + ' 50%, transparent 50%)',
-      'linear-gradient(' + fg + ' 50%, transparent 50%)'
-    ],
+    'background-image:' +
+      [
+        'linear-gradient(90deg, ' + fg + ' 50%, transparent 50%)',
+        'linear-gradient(' + fg + ' 50%, transparent 50%)'
+      ],
     'background-size: ' + size + 'px ' + size + 'px'
-  ].join(';');
+  ].join(';')
 }
 
 function zigzag(id, fg, bg) {
-  var size = 1 + (id % 180);
-  var dbl = size * 2;
+  var size = 1 + id % 180
+  var dbl = size * 2
 
-  fg = fg.replace(/hsl/, 'hsla').replace(/\)/, ', 0.5)');
+  fg = fg.replace(/hsl/, 'hsla').replace(/\)/, ', 0.5)')
 
   return [
-    'background:' + [
-      'linear-gradient(135deg, ' + fg + ' 25%, transparent 25%) -' + size + 'px 0',
-      'linear-gradient(225deg, ' + fg + ' 25%, transparent 25%) -' + size + 'px 0',
-      'linear-gradient(315deg, ' + fg + ' 25%, transparent 25%)',
-      'linear-gradient(45deg, ' + fg + ' 25%, transparent 25%)'
-    ],
+    'background:' +
+      [
+        'linear-gradient(135deg, ' +
+          fg +
+          ' 25%, transparent 25%) -' +
+          size +
+          'px 0',
+        'linear-gradient(225deg, ' +
+          fg +
+          ' 25%, transparent 25%) -' +
+          size +
+          'px 0',
+        'linear-gradient(315deg, ' + fg + ' 25%, transparent 25%)',
+        'linear-gradient(45deg, ' + fg + ' 25%, transparent 25%)'
+      ],
     'background-color: ' + bg,
     'background-size: ' + dbl + 'px ' + dbl + 'px'
-  ].join(';');
+  ].join(';')
 }
