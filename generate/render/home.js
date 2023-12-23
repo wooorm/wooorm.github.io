204d119b7359eed09c91a1ed4c0764bd58cc0615
Titus Wormer <tituswormer@gmail.com>
2023-12-23 10:50:39 +0100

    Update some stuff on home page

--- generate/render/home.js
+++ generate/render/home.js
@@ -9,28 +9,26 @@ import {h} from 'hastscript'
 export const data = {
   description: 'A place for Titus on the web',
   label: 'home',
-  modified: '2023-04-27T00:00:00.000Z',
+  modified: '2023-12-23T00:00:00.000Z',
   pathname: '/',
   published: '2020-05-01T00:00:00.000Z',
   title: '@wooorm'
 }
 
 const backend = 'https://github.com/cmda-be/course-17-18'
-const books = 'https://twitter.com/SparksZilla/status/1222208487351971840'
 const downloads = 'https://npm-stat.com/charts.html?author=wooorm'
 const email = 'mailto:tituswormer@gmail.com'
 const github = 'https://github.com/wooorm'
 const masto = 'https://mastodon.social/@wooorm'
-const mdxsm = 'https://github.com/micromark/mdx-state-machine'
 const mm = 'https://github.com/micromark/micromark'
+const markdownRs = 'https://github.com/wooorm/markdown-rs'
+const mdxjsRs = 'https://github.com/wooorm/mdxjs-rs'
 const projects =
   'https://github.com/search?type=Repositories&q=user:wooorm+user:mdx-js+user:micromark+user:remarkjs+user:rehypejs+user:retextjs+user:unifiedjs+user:syntax-tree+user:vfile+user:get-alex+user:words'
 const resume = '/resume.html'
 const thanks = '/thanks/'
 const tt = 'https://github.com/cmda-tt/course-18-19'
 const twitter = 'https://twitter.com/wooorm'
-const unified = 'https://unifiedjs.com'
-const vc = 'https://vercel.com'
 
 /** @type {Render} */
 export function render() {
@@ -44,11 +42,9 @@ export function render() {
           ' maintaining ',
           h('a', {href: projects}, '550+ projects'),
           ' that are downloaded ',
-          h('a', {href: downloads}, '22B+ times'),
+          h('a', {href: downloads}, '23B+ times'),
           ' a year. ',
-          'I can spend time as a core team member of ',
-          h('a', {href: unified}, 'unified'),
-          ', building things for content (natural language, markdown, markup) with syntax trees, ',
+          'I can spend time building things for content (natural language, markdown, markup) with syntax trees, ',
           h('a', {href: thanks}, 'thanks'),
           ' to the community.'
         ])
@@ -56,15 +52,14 @@ export function render() {
       h(
         'p',
         h('span.text', [
-          'I sometimes do contracting as well. Previously for ',
-          h('a', {href: vc}, 'Vercel'),
-          ', ',
-          h('a', {href: mm}, 'Salesforce who funded part of micromark'),
-          ', ',
-          h('a', {href: mdxsm}, 'Gatsby speccing MDX'),
-          ', and ',
-          h('a', {href: books}, 'Holloway making books'),
-          '. ',
+          'I sometimes do contracting as well. ',
+          'Previously for Vercel on ',
+          h('a', {href: markdownRs}, h('code', 'markdown-rs')),
+          ' and ',
+          h('a', {href: mdxjsRs}, h('code', 'mdxjs-rs')),
+          ', Salesforce on parts of ',
+          h('a', {href: mm}, h('code', 'micromark')),
+          ', Holloway on books, and Gatsby. ',
           'I used to teach ',
           h('a', {href: tt}, 'data visualisation, frontend'),
           ', and ',
